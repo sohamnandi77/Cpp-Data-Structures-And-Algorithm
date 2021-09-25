f29a14760964103a5b58cfff925cd8f7ed5aa6c1
Soham Nandi <56152437+sohamnandi77@users.noreply.github.com>
2021-09-25 20:53:53 +0530

    Update 07. SumOfDigits.cpp

--- Recursion/Recursion - 1 - Basics/07. SumOfDigits.cpp
+++ Recursion/Recursion - 1 - Basics/07. SumOfDigits.cpp
@@ -2,9 +2,7 @@
 using namespace std;
 int sumOfDigits(int n)
 {
-    if (n <= 10)
-        return n;
-    return n % 10 + sumOfDigits(n / 10);
+    return n<10 ? n : n % 10 + sumOfDigits(n/10);
 }
 int main()
 {
